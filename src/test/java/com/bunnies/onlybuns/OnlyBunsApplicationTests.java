2d02ca5621e9618dde740f50bd3dcb6664656504
Anđela <a.radojevic01@gmail.com>
2024-10-14 23:33:22 +0200

    Initial project

---
+++ src/test/java/com/bunnies/onlybuns/OnlyBunsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunnies.onlybuns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyBunsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
